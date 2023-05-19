5e323677b8068ca22c455e0b634a223c3c27e4cb
AkinDagba <akindagba@gmail.com>
2023-05-19 15:35:43 -0300

    Segundo Commit

--- GCMSPraticaGit/src/javapack/HelloWorld.java
+++ GCMSPraticaGit/src/javapack/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		
-		System.out.printf("Hello World !");// TODO Auto-generated method stub
+		System.out.printf("Hello World !!");// TODO Auto-generated method stub
 
 	}
 
